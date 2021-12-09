b568af2095a90d3d9b3eef31d4ffa3bffdf06a3f
1peded1 <krekota.st@gmail.com>
2021-12-09 22:21:29 +0200

    1

--- .vs/1.cpp
+++ .vs/1.cpp
@@ -1,26 +1,21 @@
-// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include "windows.h"
+using namespace std;
 
 int main()
 {
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 
-    std::cout << "�����, ��������!\n";
-    system("pause");
-    return 0;
+	int x, a, b;
+	cout << "\n������ �: ";
+	cin >> x;
+	cout << "\n������ A: ";
+	cin >> a;
+	cout << "\n������ B: ";
+	cin >> b;
+	cout << "\n ����� A � ������ ��� ����������� ?   " << a % 2 && a < 10 << endl;
+	system("pause");
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
